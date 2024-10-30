7fb64448f986ec03b39901da446d6de3dfcbb2b8
t23b227a <t23b227a@mail.cc.niigata-u.ac.jp>
2024-10-30 16:30:36 +0900

    debug用

--- removeAD.js
+++ removeAD.js
@@ -1,5 +1,13 @@
 let ad1 =document.querySelectorAll('div[id*="google_ads_iframe"]');
+
 ad1.forEach(div =>{
     div.classList.add('GoogleADs');
     console.log(div);
 }) ;
+
+// debug用
+document.addEventListener('click', () => {
+    Array.from(ad1).map(ad => {
+        console.log(ad);
+    })
+});
