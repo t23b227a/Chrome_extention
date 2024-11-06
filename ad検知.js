b8e6f8e7ceb08e147c4a3bf1b03b95bd9cbf6ea8
Sota <breeze.prunus0707@gmail.com>
2024-11-06 16:33:08 +0900

    検知＋削除（全部消える）

--- ad検知.js
+++ ad検知.js
@@ -1,21 +1,39 @@
-let ad1 =document.querySelectorAll('div[id*="ad"]');
-const CrossImage = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiJXYVCbpXdstc30mEWtspHcixWtjN83WZdccPF9QNtF2S9Bykwp5TcMVT8jB4FNEBModDyO_HR5BYIYCvqg_VzEXhbKy7gymQU35n5cpfBr53L_5l9rNqiiz6yR-D1aAOMlpdsvqgXMlI6/s800/mark_batsu.png"
-ad1.forEach(div =>{
+let ad1 = document.querySelectorAll('div[id*="ad"]');
+const CrossImage = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiJXYVCbpXdstc30mEWtspHcixWtjN83WZdccPF9QNtF2S9Bykwp5TcMVT8jB4FNEBModDyO_HR5BYIYCvqg_VzEXhbKy7gymQU35n5cpfBr53L_5l9rNqiiz6yR-D1aAOMlpdsvqgXMlI6/s800/mark_batsu.png";
+
+const img = document.createElement('img');
+img.src = CrossImage;
+img.style.position = 'absolute';
+img.style.display = 'none';
+img.style.width = '200px';
+img.style.height = '200px';
+document.body.appendChild(img);
+
+ad1.forEach(div => {
     div.classList.add('GoogleADs');
-    const img = document.createElement('img');
-    img.src = CrossImage;
-    img.style.position = 'absolute';
-    img.style.display = 'none';
-    img.style.width = '200px';
-    img.style.height = '200px';
-    img.style.left ='1000';
-    img.style.top ='100';
-    document.body.appendChild(img);
+    
     div.addEventListener("mouseover", function () {
+        const rect = div.getBoundingClientRect();
+        img.style.left = `${rect.left + window.scrollX}px`;
+        img.style.top = `${rect.top + window.scrollY}px`;
         img.style.display = 'block';
     }, false);
-    addEventListener("mouseout", function() {
+    
+    div.addEventListener("mouseout", function() {
         img.style.display = 'none';
     }, false);
+    
+    // 画像に対してマウスオーバー時のイベントを追加
+    img.addEventListener("mouseover", function() {
+        img.style.display = 'block'; // 画像が見えている場合は常に表示
+    }, false);
+    
+    img.addEventListener("mouseout", function() {
+        img.style.display = 'none'; // マウスが外れたときに非表示
+    }, false);
+
+    img.addEventListener("click", function() {
+        div.remove(); // 画像がクリックされたら広告を削除
+    });
     console.log(div);
-}) ;
+});
