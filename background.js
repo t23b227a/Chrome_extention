e06a1823e1c718f5fd74707341b9bc5e22cc9a2d
t23b227a <t23b227a@mail.cc.niigata-u.ac.jp>
2024-11-13 15:39:35 +0900

    メニューテスト2

--- background.js
+++ background.js
@@ -12,27 +12,18 @@ chrome.runtime.onInstalled.addListener(() => {
 chrome.contextMenus.onClicked.addListener((info, tab) => {
     console.log('test');
     console.log('info.menuItemId', info.menuItemId);
-    console.log('tabId', tabId);
-
-    // if (info.menuItemId === 'remove_element') {
-    //     chrome.scripting.executeScript({
-    //         target: { tabId: tab.id },
-    //         func: removeElement,
-    //     });
-    // }
-
-    // Background Scirptからはコンテストメニュー表示元の要素を直接取得できないため、
-    // Message通知機能を使って、Content Scirpt側にメッセージを送信する
-    chrome.tabs.sendMessage(tab.id, "getClickedEl", (response) => {
-        // Content Scirpt側で設定されたレスポンス（クリックされた要素）が
-        // コールバック関数の引数でやってくる
-        console.log(response.value);
-    });
+    console.log('tabId', tab.Id);
+    if (info.menuItemId === 'remove_element') {
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            func: removeElement,
+        });
+    }
 });
 
 const removeElement = () => {
     chrome.tabs.sendMessage(tab.id, "getClickedEl", response => {
-        // response.value.remove();
         console.log('response', response.value);
+        response.value.remove();
     });
 }
