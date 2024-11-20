89a0bf5698c59eabaed973ecb91da54a5f4d0759
t23b227a <t23b227a@mail.cc.niigata-u.ac.jp>
2024-11-20 15:20:21 +0900

    popupの修正

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,9 @@
-console.log('popup.js');
+//スイッチの外枠とスイッチの要素を取得
+const switchOuter = document.querySelector(".switch_outer");
+const toggleSwitch = document.querySelector(".toggle_switch");
+
+//クリックでacitveクラスを追加/削除
+switchOuter.addEventListener('click', () => {
+    switchOuter.classList.toggle("active");
+    toggleSwitch.classList.toggle("active");
+});
